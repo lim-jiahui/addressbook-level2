1494b364caf157a13056c8b7d5879c2a3abef731
lim-jiahui <e0035327@u.nus.edu>
2019-02-12 17:03:14 +0800

    Add confirmation before clearing addressbook.

--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -1,5 +1,7 @@
 package seedu.addressbook.commands;
 
+import java.util.Scanner;
+
 /**
  * Clears the address book.
  */
@@ -10,10 +12,26 @@ public class ClearCommand extends Command {
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_CONFIRMATION = "Are you sure you want to clear all contacts? Y/N";
+    public static final String MESSAGE_CANCELLED = "Cancelled, address book has not been cleared.";
+    public static final String MESSAGE_INVALID = "Invalid input, address book has not been cleared.";
 
     @Override
     public CommandResult execute() {
-        addressBook.clear();
-        return new CommandResult(MESSAGE_SUCCESS);
+        System.out.println(MESSAGE_CONFIRMATION);
+        Scanner scan = new Scanner(System.in);
+        String confirmClear = scan.nextLine();
+
+        if (confirmClear == "Y" || confirmClear == "y") {
+            addressBook.clear();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            if (confirmClear == "N" || confirmClear == "n") {
+                return new CommandResult(MESSAGE_CANCELLED);
+            }
+        }
+
+        System.out.println(MESSAGE_INVALID);
+        return execute();
     }
 }
